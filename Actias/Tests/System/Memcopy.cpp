960e1fda68075481b67b65c705cdec6151af2f17
n-dub <50988318+n-dub@users.noreply.github.com>
2023-11-05 18:14:25 +0500

    Fix memcpy tests

--- Actias/Tests/System/Memcopy.cpp
+++ Actias/Tests/System/Memcopy.cpp
@@ -2,28 +2,43 @@
 #include <Actias/Time/DateTime.hpp>
 #include <gtest/gtest.h>
 
-inline constexpr USize MaxMemcopyTestSize = 16 * 1024;
+inline constexpr USize OverflowCheckCount = 4;
+inline constexpr USize BufferAlignment    = 256;
+inline constexpr USize MaxMemcopyTestSize = 64 * 1024;
 
-static alignas(32) Int32 g_Source[MaxMemcopyTestSize];
-static alignas(32) Int32 g_Destination[MaxMemcopyTestSize];
+static alignas(BufferAlignment) UInt8 g_Source[MaxMemcopyTestSize + OverflowCheckCount];
+static alignas(BufferAlignment) UInt8 g_Destination[MaxMemcopyTestSize + OverflowCheckCount];
 
 using FMemcpy = decltype(ActiasCopyMemory);
 
 inline void TestMemcopy(USize bufferSize, FMemcpy* proc)
 {
-    auto time = Actias::DateTime::UtcNow();
+    const auto time = Actias::DateTime::UtcNow();
     srand(static_cast<unsigned int>(time.Ticks()));
 
     for (USize i = 0; i < bufferSize; ++i)
     {
-        g_Source[i] = rand();
+        g_Source[i] = static_cast<UInt8>(rand() & 0xFF);
     }
 
-    proc(g_Destination, g_Source, bufferSize * 4);
+    for (USize i = 0; i < OverflowCheckCount; ++i)
+    {
+        g_Source[bufferSize + i]      = 0xFF;
+        g_Destination[bufferSize + i] = 0;
+    }
+
+    proc(g_Destination, g_Source, bufferSize);
 
     for (USize i = 0; i < bufferSize; ++i)
     {
-        ASSERT_EQ(g_Destination[i], g_Source[i]) << "Size = " << bufferSize << "; i = " << i;
+        ASSERT_EQ(g_Destination[i], g_Source[i])
+            << "Size = " << bufferSize << " bytes; i = " << i;
+    }
+
+    for (USize i = 0; i < OverflowCheckCount; ++i)
+    {
+        ASSERT_EQ(g_Destination[bufferSize + i], 0)
+            << "Overflow error; Size = " << bufferSize << "bytes; i = " << i;
     }
 }
 
@@ -53,7 +68,7 @@ TEST(CopyMemory, NormalCopy)
 
 TEST(CopyMemory, AlignedCopy)
 {
-    for (USize i = 1024; i < 4096; i += 32)
+    for (USize i = 1024; i < 4096; i += BufferAlignment)
     {
         TestMemcopy(i, ActiasCopyAlignedMemory);
     }
