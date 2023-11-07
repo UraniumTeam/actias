2ceef82e7552db0319da8cc433e3bc9b456ea3f3
n-dub <50988318+n-dub@users.noreply.github.com>
2023-11-07 18:36:47 +0500

    Add import descriptors to PE-specific headers

---
+++ ActiasSDK/ActiasSDK/Platform/Windows/Parser/PEDataDirectory.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <Actias/Base/Base.hpp>
+#include <Actias/Base/Byte.hpp>
+#include <ActiasSDK/Platform/Windows/Parser/PECommon.hpp>
+
+namespace Actias::SDK::PE
+{
+    struct DataDirectory
+    {
+        PEVirtualAddress VirtualAddress;
+        UInt32 Size;
+    };
+
+    struct ExportDirectory
+    {
+        UInt32 Characteristics;
+        UInt32 TimeDateStamp;
+        UInt16 MajorVersion;
+        UInt16 MinorVersion;
+        UInt32 Name;
+        UInt32 Base;
+        UInt32 NumberOfFunctions;
+        UInt32 NumberOfNames;
+        UInt32 AddressOfFunctions;    //!< RVA from base of image.
+        UInt32 AddressOfNames;        //!< RVA from base of image.
+        UInt32 AddressOfNameOrdinals; //!< RVA from base of image.
+    };
+
+    struct ImportDescriptor
+    {
+        UInt32 OriginalFirstThunk; // RVA to original unbound IAT (PIMAGE_THUNK_DATA).
+        UInt32 TimeDateStamp;
+        UInt32 ForwarderChain;
+        UInt32 Name;
+        UInt32 FirstThunk; // RVA to IAT.
+    };
+} // namespace Actias::SDK::PE
