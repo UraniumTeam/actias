253be3256db3b919280c239f62c1e38e79321b7b
n-dub <ragucoz@gmail.com>
2023-11-07 18:31:24 +0500

    Fix atomic function names

--- Actias/Actias/System/Platform/Common/Atomic.asm
+++ Actias/Actias/System/Platform/Common/Atomic.asm
@@ -1,28 +1,28 @@
 section .text
 bits 64
 
-global ActiasAtomicAddI8, ActiasAtomicAddI16, ActiasAtomicAddI32, ActiasAtomicAddI64
+global ActiasAtomicAddI8@@16, ActiasAtomicAddI16@@16, ActiasAtomicAddI32@@16, ActiasAtomicAddI64@@16
 
-export ActiasAtomicAddI8
-ActiasAtomicAddI8:
+export ActiasAtomicAddI8@@16
+ActiasAtomicAddI8@@16:
     mov     eax,  DWORD edx
     lock    xadd  BYTE [rcx], al
     ret
 
-export ActiasAtomicAddI16
-ActiasAtomicAddI16:
+export ActiasAtomicAddI16@@16
+ActiasAtomicAddI16@@16:
     mov     eax,  DWORD edx
     lock    xadd  WORD [rcx], ax
     ret
 
-export ActiasAtomicAddI32
-ActiasAtomicAddI32:
+export ActiasAtomicAddI32@@16
+ActiasAtomicAddI32@@16:
     mov     eax,  DWORD edx
     lock    xadd  DWORD [rcx], eax
     ret
 
-export ActiasAtomicAddI64
-ActiasAtomicAddI64:
+export ActiasAtomicAddI64@@16
+ActiasAtomicAddI64@@16:
     mov     rax,  QWORD rdx
     lock    xadd  QWORD [rcx], rax
     ret
