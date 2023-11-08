6244ce2d2bd533fafb76e9dabba2bc66affbb0d4
n-dub <50988318+n-dub@users.noreply.github.com>
2023-11-08 17:18:50 +0500

    FIx assembly on Windows

--- Actias/Actias/System/Platform/Common/Atomic.asm
+++ Actias/Actias/System/Platform/Common/Atomic.asm
@@ -3,24 +3,28 @@ section .text
 bits 64
 
 ACTIAS_DLL_EXPORT(ActiasAtomicAddI8@@16)
+global ActiasAtomicAddI8@@16
 ActiasAtomicAddI8@@16:
     mov     eax,  DWORD edx
     lock    xadd  BYTE [rcx], al
     ret
 
 ACTIAS_DLL_EXPORT(ActiasAtomicAddI16@@16)
+global ActiasAtomicAddI16@@16
 ActiasAtomicAddI16@@16:
     mov     eax,  DWORD edx
     lock    xadd  WORD [rcx], ax
     ret
 
 ACTIAS_DLL_EXPORT(ActiasAtomicAddI32@@16)
+global ActiasAtomicAddI32@@16
 ActiasAtomicAddI32@@16:
     mov     eax,  DWORD edx
     lock    xadd  DWORD [rcx], eax
     ret
 
 ACTIAS_DLL_EXPORT(ActiasAtomicAddI64@@16)
+global ActiasAtomicAddI64@@16
 ActiasAtomicAddI64@@16:
     mov     rax,  QWORD rdx
     lock    xadd  QWORD [rcx], rax
