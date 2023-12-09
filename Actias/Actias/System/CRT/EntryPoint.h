2414f6d48110a8a37deffadddae172c163ed6214
n-dub <50988318+n-dub@users.noreply.github.com>
2023-12-09 22:25:13 +0500

    call `ActiasLibraryMain`

--- Actias/Actias/System/CRT/EntryPoint.h
+++ Actias/Actias/System/CRT/EntryPoint.h
@@ -3,7 +3,23 @@
 
 ACTIAS_BEGIN_C
 
-typedef Int32 ACTIAS_ABI ActiasMainProc(const char*);
-typedef Int32 ACTIAS_ABI ActiasLibraryMainProc();
+enum ActiasMainCallReasonValues
+{
+    ACTIAS_MAIN_CALL_REASON_NONE = 0x00,
+
+    ACTIAS_MAIN_CALL_REASON_LIBRARY_LOAD   = 0x11,
+    ACTIAS_MAIN_CALL_REASON_LIBRARY_UNLOAD = 0x12,
+
+    ACTIAS_MAIN_CALL_REASON_EXECUTABLE_START = 0x21,
+    ACTIAS_MAIN_CALL_REASON_EXECUTABLE_EXIT  = 0x22
+};
+
+typedef Int32 ActiasMainCallReason;
+
+#define ActiasMain_ProcName "ActiasMain"
+#define ActiasLibraryMain_ProcName "ActiasLibraryMain"
+
+typedef Int32 ACTIAS_ABI ActiasMainProc(ActiasMainCallReason, const char*);
+typedef ActiasResult ACTIAS_ABI ActiasLibraryMainProc(ActiasMainCallReason, ActiasHandle);
 
 ACTIAS_END_C
--- Actias/Actias/System/CRT/Windows/EntryPointExe.c
+++ Actias/Actias/System/CRT/Windows/EntryPointExe.c
@@ -1,9 +1,13 @@
+#include <Actias/System/CRT/EntryPoint.h>
 #include <Actias/System/Platform/Windows/WinHeaders.h>
 
-int __cdecl main(int argc, LPCSTR argv[]);
+ActiasMainProc ActiasMain;
+
+int __cdecl main(int argc, char** argv);
 
 VOID wmainCRTStartup()
 {
     int exitCode = main(0, NULL);
+    // int exitCode = ActiasMain(ACTIAS_MAIN_CALL_REASON_EXECUTABLE_START, GetCommandLineA());
     ExitProcess(exitCode);
 }
