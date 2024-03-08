d6fbff8618adea19da762aaed0bc50a18de38f6b
n-dub <50988318+n-dub@users.noreply.github.com>
2024-03-08 15:47:04 +0500

    #27 Kernel (Process environment) for single global state. The Kernel object now holds the global Runtime state: - Reference counters for each loaded library; - Additional library metadata which can be useful in the future.

--- ActiasRuntime/ActiasRuntime/Builder/ModuleBuilder.cpp
+++ ActiasRuntime/ActiasRuntime/Builder/ModuleBuilder.cpp
@@ -105,21 +105,20 @@ namespace Actias::Runtime
         return OK{};
     }
 
-    Result<ActiasHandle, ResultCode> ModuleBuilder::Build()
+    Result<ModuleInfo, ResultCode> ModuleBuilder::Build()
     {
         ActiasSystemProperties systemProperties;
         ActiasGetSystemProperties(&systemProperties);
 
         const auto sectionBaseAddress = AlignUp(m_TotalHeaderSize, systemProperties.PageSize);
 
-        auto lastSection = m_Sections.Back();
-        auto imageSize   = AlignUp(sectionBaseAddress + lastSection->Address + lastSection->Size, systemProperties.PageSize);
+        const auto lastSection = m_Sections.Back();
+        const auto imageSize = AlignUp(sectionBaseAddress + lastSection->Address + lastSection->Size, systemProperties.PageSize);
 
         void* handle = ActiasVirtualAlloc(nullptr, imageSize, ACTIAS_MEMORY_PROTECTION_READ_WRITE);
         ActiasCopyMemory(handle, m_RawData.Data(), m_TotalHeaderSize);
 
-        m_pMapped                     = ac_byte_cast(handle);
-        *static_cast<UInt64*>(handle) = static_cast<UInt64>(imageSize);
+        m_pMapped = ac_byte_cast(handle);
 
         for (auto* section : m_Sections)
         {
@@ -140,7 +139,11 @@ namespace Actias::Runtime
             ActiasCopyMemory(m_pMapped + section->Address, pRawSection, section->RawSize);
         }
 
-        return static_cast<ActiasHandle>(handle);
+        ModuleInfo moduleInfo{};
+        moduleInfo.ImageSize        = static_cast<UInt64>(imageSize);
+        moduleInfo.Handle           = static_cast<ActiasHandle>(handle);
+        moduleInfo.ReferenceCounter = 1;
+        return moduleInfo;
     }
 
     VoidResult<ResultCode> ModuleBuilder::ImportAll()
---
+++ ActiasRuntime/ActiasRuntime/Kernel/RuntimeKernel.hpp
@@ -0,0 +1,71 @@
+#pragma once
+#include <Actias/Strings/StringSlice.hpp>
+#include <Actias/System/Mutex.h>
+#include <ActiasRuntime/Base/Base.hpp>
+
+namespace Actias::Runtime
+{
+    //! \brief Loaded runtime module information.
+    struct ModuleInfo
+    {
+        String Name;                 //!< Module name.
+        ActiasHandle Handle     = 0; //!< Module handle.
+        UInt64 ImageSize        = 0; //!< Size of the loaded image, used for deallocation.
+        UInt32 ReferenceCounter = 0; //!< Number of references to this module, incremented by ActiasLoadModule.
+    };
+
+    namespace Kernel
+    {
+        //! \brief Runtime Kernel-specific scoped lock.
+        class LockScope final
+        {
+            friend class Impl;
+
+            ActiasMutex* m_pMutex;
+            bool* m_pOwnerFlag;
+
+            LockScope(const LockScope&)            = delete;
+            LockScope& operator=(const LockScope&) = delete;
+
+            inline LockScope(ActiasMutex* pMutex, bool* pOwnerFlag)
+                : m_pMutex(pMutex)
+                , m_pOwnerFlag(pOwnerFlag)
+            {
+                ActiasLockMutex(pMutex);
+                *m_pOwnerFlag = true;
+            }
+
+        public:
+            LockScope(LockScope&& other) noexcept
+                : m_pMutex(other.m_pMutex)
+                , m_pOwnerFlag(other.m_pOwnerFlag)
+            {
+                other.m_pMutex     = nullptr;
+                other.m_pOwnerFlag = nullptr;
+            }
+
+            ~LockScope()
+            {
+                if (m_pMutex)
+                {
+                    ActiasUnlockMutex(m_pMutex);
+                    if (m_pOwnerFlag)
+                    {
+                        *m_pOwnerFlag = false;
+                    }
+                }
+            }
+        };
+
+        LockScope AquireLock();
+
+        //! \brief Returns true if the module was found.
+        bool TryGetModule(const StringSlice& name, ModuleInfo& moduleInfo);
+
+        //! \brief Add a new module.
+        void AddModule(ModuleInfo* pModuleInfo);
+
+        //! \brief Returns true if the module must be unloaded.
+        bool RemoveModuleReference(const ModuleInfo& moduleInfo);
+    } // namespace Kernel
+} // namespace Actias::Runtime
