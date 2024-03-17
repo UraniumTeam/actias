eab7f9b4e218ca3e9cc131ff6112935383efc647
Daniil Lapygin <lapygin_d@mail.ru>
2024-03-17 14:40:15 +0500

    Fixed ActiasWindow and ActiasWindowMonitor structs. Added glfw build options

--- Actias/Actias/System/Platform/Common/Window.c
+++ Actias/Actias/System/Platform/Common/Window.c
@@ -1,10 +1,10 @@
 #include <Actias/System/Window.h>
-#include <stdlib.h>
+//#include <stdlib.h>
 
 ActiasWindow ACTIAS_ABI ActiasCreateWindow(Int32 width, Int32 height, const char* title, ActiasWindowMonitor monitor,
                                            ActiasWindow share)
 {
-    glfwCreateWindow(width, height, title, monitor.monitor, share.window);
+    return (ActiasWindow)glfwCreateWindow(width, height, title, (GLFWmonitor*)monitor, (GLFWwindow*)share);
 }
 
 void ACTIAS_ABI ActiasPollEvents()
@@ -14,7 +14,7 @@ void ACTIAS_ABI ActiasPollEvents()
 
 void ACTIAS_ABI ActiasDestroyWindow(ActiasWindow window)
 {
-    glfwDestroyWindow(window.window);
+    glfwDestroyWindow((GLFWwindow*)window);
 }
 
 void ACTIAS_ABI ActiasTerminate()
